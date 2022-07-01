9a08e20b63bf2bc1282855c040c1681939ac1d66
오성환 <kooju0704@naver.com>
2022-07-01 15:14:33 +0900

    ADD::RockScissorsPaper

--- testJava/src/test.java
+++ testJava/src/test.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
+class rockScissorsPaper{
+    int computer, player;
+    public void setValue(int computer, int player){
+        this.computer = computer;
+        this.player = player;
+    }
+    public void whoWinner(){
+        if (this.computer == this.player) System.out.println("DRAW!!");
+        else if (this.computer == 1 && this.player == 2) System.out.println("WIN!!");
+        else if (this.computer == 1 && this.player == 3) System.out.println("LOSE!!");
+        else if (this.computer == 2 && this.player == 1) System.out.println("LOSE!!");
+        else if (this.computer == 2 && this.player == 3) System.out.println("WIN!!");
+        else if (this.computer == 3 && this.player == 1) System.out.println("WIN!!");
+        else if (this.computer == 3 && this.player == 2) System.out.println("LOSE!!");
+    }
+}
 public class test {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
+        Scanner scanner = new Scanner (System.in);
+        System.out.println("1(가위), 2(바위), 3(보) 중 하나를 입력해주세요");
+        int player = scanner.nextInt();
+        int computer = (int)(Math.random() * 3) + 1;
+        rockScissorsPaper whoWinner = new rockScissorsPaper();
+        whoWinner.setValue(computer,player);
+        whoWinner.whoWinner();
     }
 }
