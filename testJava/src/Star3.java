bddf758df250a593a8fc0ba5c751f416b3c72194
오성환 <kooju0704@naver.com>
2022-07-04 22:07:04 +0900

    ADD::iCountingStar

---
+++ testJava/src/Star3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Star3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for(int i = 0; i < num; i++){
+            for(int j = 0; j < i; j++){
+                System.out.print(" ");
+            }
+            for(int j = 0; j < num * 2 - (i * 2 + 1); j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
