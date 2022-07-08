0759386271127d7a1c7eb3381124d4a8e80da71a
오성환 <kooju0704@naver.com>
2022-07-08 22:06:10 +0900

    ADD::jungol_144

--- testJava/src/jungol_144.java
+++ testJava/src/jungol_144.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
-public class Star2 {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+public class jungol_144 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner (System.in);
         int n = scanner.nextInt();
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n - i - 1; j++){
+            for(int j = 0; j < n * 2 - i * 2 - 2; j++){
                 System.out.print(" ");
             }
-            for(int j = 0; j < i * 2 + 1; j++){
+            for(int j = n; j > n - i * 2 - 1; j--){
                 System.out.print("*");
             }
             System.out.println();
