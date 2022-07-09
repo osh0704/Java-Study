302943b6c0399f3a25620e5987e19919498162c3
오성환 <kooju0704@naver.com>
2022-07-09 22:08:25 +0900

    ADD::jungol_145

--- testJava/src/jungol_145.java
+++ testJava/src/jungol_145.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class jungol_147 {
+public class jungol_145 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int inputNumber = scanner.nextInt();
-        int cnt = 1;
         for(int i = 0; i < inputNumber; i++){
-            for(int j = 0; j < i * 2; j++){
+            for(int j = 0; j < inputNumber - i; j++){
                 System.out.print(" ");
             }
-            for(int j = 0; j < inputNumber - i; j++){
-                System.out.print(cnt++ + " ");
+            for(int j = 1; j <= i + 1; j++){
+                System.out.print(j + " ");
             }
             System.out.println();
         }
