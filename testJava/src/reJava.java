2a46160f990cbaaa47ded6f9ed4aed6578eb301c
오성환 <kooju0704@naver.com>
2022-09-13 19:11:27 +0900

    ADD :: OOP_EX

--- testJava/src/reJava.java
+++ testJava/src/reJava.java
@@ -1,16 +1,34 @@
 import org.w3c.dom.ls.LSOutput;
 
 import java.util.Scanner;
-public class reJava {
-    public static void main(String[] args) {
-        String[] name = new String[5];
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 5; i++)
-            name[i] = scanner.nextLine();
 
-        for(int i = 0; i < 5; i++){
-            System.out.println(name[i]);
-            System.out.println(name[i].length());
-        }
+class Calculator {
+    int left, right;
+    public void setOperator(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+
+    public void sum() {
+        System.out.println(this.left + this.right);
+    }
+    public void avg(){
+        System.out.println((this.left + this.right) / 2);
+    }
+}
+
+class SubstractionableCalculator extends Calculator{
+    public void substract(){
+        System.out.println(this.left - this.right);
+    }
+}
+
+public class reJava {
+    public static void main (String[] args){
+        SubstractionableCalculator c2 = new SubstractionableCalculator();
+        c2.setOperator(124,23);
+        c2.avg();
+        c2.sum();
+        c2.substract();
     }
 }
